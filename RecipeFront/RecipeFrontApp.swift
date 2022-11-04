ea655fab05a91cc97329079ea604a6cb8c36e827
Hallucinette <ameliepocchiolo42M@gmail.com>
2022-11-04 12:24:19 +0100

    creation fichier

---
+++ RecipeFront/RecipeFrontApp.swift
@@ -0,0 +1,17 @@
+//
+//  RecipeFrontApp.swift
+//  RecipeFront
+//
+//  Created by Amelie Pocchiolo on 04/11/2022.
+//
+
+import SwiftUI
+
+@main
+struct RecipeFrontApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
